bd99e51819c45df2408de03ff564037875912a2f
hwk9407 <hwk__@naver.com>
2024-09-06 14:25:13 +0900

    fix(Calculator): calculate메서드 로직 변경

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,76 +1,57 @@
 package calculator;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
 
+import java.util.ArrayList;
+
+// 사칙연산을 수행 후,
+// 결과값 반환 메서드 구현
+// 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스를 생성
 public class Calculator {
+    private ArrayList<Double> resultHistory;
+    
+    // 생성자. 필드 초기화
+    Calculator() {
+        this.resultHistory = new ArrayList<>();
+    }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public double calculate(int a, int b, char operator) {
+        double result;
 
-        // 2개의 양의 정수 0포함 입력 받기
-        while (true) {
-            int a, b;
-
-            while (true) {
-                try {
-                    System.out.println("첫 번째 0 또는 양의 정수를 입력해주세요.");
-                    a = sc.nextInt();
-                    if (a < 0) {
-                        System.out.println("음수를 입력 하셨습니다.");
-                        continue;
-                    }
-                    break;
-                } catch (InputMismatchException e) { // int가 아닐 때
-                    System.out.println("숫자를 입력해주세요!");
-                    sc.nextLine();  // 입력 버퍼 비우기
-                }
-            }
-
-            while (true) {
-                try {
-                    System.out.println("두 번째 0 또는 양의 정수를 입력해주세요.");
-                    b = sc.nextInt();
-                    if (b < 0) {
-                        System.out.println("음수를 입력 하셨습니다.");
-                        continue;
-                    }
-                    break;
-                } catch (InputMismatchException e) { // int가 아닐 때
-                    System.out.println("숫자를 입력해주세요!");
-                    sc.nextLine();  // 입력 버퍼 비우기
-                }
-            }
-
-
-            // 사칙 연산 기호 입력 받기
-            System.out.println("사칙연산 기호를 입력해주세요.");
-            char operator = sc.next().charAt(0);
-
-            if (operator == '+') {
-                System.out.println(a + " " + operator + " " + b + " = " + (a + b));
-            } else if (operator == '-') {
-                System.out.println(a + " " + operator + " " + b + " = " + (a - b));
-            } else if (operator == '*') {
-                System.out.println(a + " " + operator + " " + b + " = " + (a * b));
-            } else if (operator == '/') {
-                if (b == 0) {
-                    System.out.println("나눗셈 연산에서는 두 번째 정수에 0이 입력될 수 없습니다.");
-                } else {
-                    System.out.println("결과 : " + a + " " + operator + " " + b + " = " + (a / b));
-                }
+        if (operator == '+') {
+            result = a + b;
+            System.out.println("결과 : " + a + " " + operator + " " + b + " = " + result);
+        } else if (operator == '-') {
+            result = a - b;
+            System.out.println("결과 : " + a + " " + operator + " " + b + " = " + result);
+        } else if (operator == '*') {
+            result = a * b;
+            System.out.println("결과 : " + a + " " + operator + " " + b + " = " + result);
+        } else if (operator == '/') {
+            if (b == 0) {
+                System.out.println("나눗셈 연산에서는 두 번째 정수에 0이 입력될 수 없습니다.");
+                // 에러 던져야함
+                return 0;
             } else {
-                System.out.println("연산 기호를 잘못 입력하였습니다.");
+                result = (double) a / b;
+                System.out.println("결과 : " + a + " " + operator + " " + b + " = " + result);
             }
-
-            sc.nextLine();  // 입력 버퍼 비우기
-            System.out.println("더 계산 하시겠습니까? (exit 입력 시 종료)");
-            String answer = sc.nextLine();
-
-            if (answer.equalsIgnoreCase("exit")) {
-                return;
-            }
+        } else {
+            System.out.println("연산 기호를 잘못 입력하였습니다.");
+            // 에러 던져야함
+            return 0;
         }
 
+        resultHistory.add(result);
+        return result;
+
+    }
+
+    // getter, setter
+    public double getFirstResult () {
+        return resultHistory.getFirst();
+    }
+
+    public void setFirstResult (double result) {
+        resultHistory.addFirst(result);
     }
 }
